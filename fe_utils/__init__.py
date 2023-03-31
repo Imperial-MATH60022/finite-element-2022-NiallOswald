bd701250d9c1d2500e1d2df7669e459f3a8a7253
Niall Oswald <niall.oswald@hotmail.co.uk>
2023-03-31 17:17:36 +0100

    Add error norm for vector finite elements

--- fe_utils/__init__.py
+++ fe_utils/__init__.py
@@ -4,4 +4,4 @@ from .mesh import Mesh, UnitSquareMesh, UnitIntervalMesh  # NOQA F401
 from .finite_elements import FiniteElement, VectorFiniteElement, LagrangeElement  # NOQA F401
 from .function_spaces import FunctionSpace, Function  # NOQA F401
 from .quadrature import gauss_quadrature  # NOQA F401
-from .utils import errornorm  # NOQA F401
+from .utils import errornorm, vector_errornorm  # NOQA F401
